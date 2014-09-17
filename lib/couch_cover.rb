26a2ed4cc66e143441f412d6dab0917a8c321e90
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-09-17 16:52:08 +0200

    initial couch_cover seminar getting functionality.

--- lib/couch_cover.rb
+++ lib/couch_cover.rb
@@ -1,5 +1,34 @@
 require "couch_cover/version"
+require 'view'
+
+require 'json'
+require 'uri'
+
+require 'rest_client'
 
 module CouchCover
-  # Your code goes here...
+  class CouchDB
+    def self.view category, name
+      View.new category, name
+    end
+
+  end
+
+  #@@couchdb_url = nil
+
+  def self.couchdb_url= new_url
+    @@couchdb_url = new_url
+  end
+
+  def self.couchdb_url
+    @@couchdb_url
+  end
+
+  def self.get_seminars opts=nil
+    if opts && opts[:from]
+      CouchDB.view('api', 'seminar_by_date').query(opts[:from])
+    else
+      CouchDB.view('api', 'seminar_by_date').query()
+    end
+  end
 end
