21230a2fa8d90109b69c697720e1048d80b59a03
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-09-18 11:16:20 +0200

    moved responsability of parameter url generation to url_parameter module.

--- lib/view.rb
+++ lib/view.rb
@@ -17,31 +17,10 @@ module CouchCover
     end
 
     def view_url startkey=nil, endkey=nil, limit=20
-      URI.encode "#{CouchCover.couchdb_url}/_design/#{@category}/_view/#{@name}#{limit_param limit}#{range startkey, endkey}"
-    end
+      params = URIParameter.construct_nonil(limit: limit,
+        startkey: startkey, endkey: endkey)
 
-    private
-
-    def limit_param limit
-      if limit && limit > 0
-        "?limit=#{limit}"
-      else
-        ""
-      end
-    end
-
-    def startkey_param startkey
-      # TODO sanitization
-      startkey ? "&startkey=#{startkey}" : ""
-    end
-
-    def endkey_param endkey
-      # TODO sanitization
-      endkey ? "&endkey=#{endkey}" : ""
-    end
-
-    def range startkey, endkey
-      "#{startkey_param startkey}#{endkey_param endkey}"
+      URI.encode "#{CouchCover.couchdb_url}/_design/#{@category}/_view/#{@name}#{params}"
     end
   end
 end
