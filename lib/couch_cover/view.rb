267b2de41ed3fa2dd4fc361427010a575589994c
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2014-11-15 17:03:10 +0100

    add accidentally ommited view.rb

---
+++ lib/couch_cover/view.rb
@@ -0,0 +1,27 @@
+require 'couch_cover'
+
+module CouchCover
+
+  class View
+    attr_accessor :category
+    attr_accessor :name
+
+    def initialize category, name
+      @category = category
+      @name = name
+    end
+
+    def query startkey=nil, endkey=nil, limit=20
+      # respect limit
+      RestClient.get view_url startkey, endkey, limit
+    end
+
+    # Assembles the URL to query (with escaping)
+    def view_url startkey=nil, endkey=nil, limit=20
+      params = URIParameter.construct_nonil(limit: limit,
+        startkey: startkey, endkey: endkey)
+
+      URI.encode "#{CouchCover.couchdb_url}/_design/#{@category}/_view/#{@name}#{params}"
+    end
+  end
+end
